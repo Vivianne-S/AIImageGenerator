35bce64810d8148107487211ef45ea8e4bc11a8e
Vivianne-s <vivvi.sonnerborg@hotmail.com>
2025-08-27 13:42:55 +0200

    Initial Commit

---
+++ AIImageGenerator/AIImageGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIImageGeneratorApp.swift
+//  AIImageGenerator
+//
+//  Created by Vivianne Sonnerborg on 2025-08-27.
+//
+
+import SwiftUI
+
+@main
+struct AIImageGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
